bc1cc0106670df3c0cc725b159097093166d90b0
Natalia Frumkin <nfrumkin@redhat.com>
2018-08-16 17:51:33 -0400

    begin documentation and add basic forecasting - reimplement basic forecasting for comparison

---
+++ documentation.md
@@ -0,0 +1,50 @@
+# Data Science on Prometheus Metrics
+## **The Metrics**
+#### Anatomy of Metrics
+For more information, visit https://prometheus.io/docs/concepts/metric_types/
+
+Gauge Metric for a given target:
+  * a time series
+
+Counter Metric for a given target:
+* a monotonically increasing time series
+
+Histogram Metric for a given target:
+* *a collection of bucket time series* -  n buckets in the histogram correspond to n time series. the +Inf bucket time series is the same as the count time series.
+* *a count time series* -
+a cumulative count of all observations we have seen thus far
+* *a sum time series* - a cumulative sum of all observations we have seen thus far
+
+Summary Metric for a given target:
+* *quantile time series* - there are n quantiles corresponding to n time series
+* *count time series* - a cumulative count of all observations we have seen thus far
+* *sum time series* - a cumulative sum of all observations we have seen thus far
+
+#### Key Insights on Metrics
+1. The value of a counter is useless - the instantaneous value of a counter is dependent on when Prometheus decides to reset the counter. it is often not indicative of the state of the system. Counters are only useful when we look at how they change over time. For this reason, when we talk about a counter we will automatically preprocess the counter into a difference series where difference(t) = raw(t) - raw(t-1)
+2. The metrics are received and stored in the form of packets. All quantile packets are stored in a quantile folder, all count packets in a count folder, etc. We parse these packets and reformat them into dictionaries for each time series type and metric. The key refers to the metadata (target info and metric contents labels) and the value is a pandas dataframe with timestamp and value for each unique metadata configuration. Essentially each key-value pair represents a single incoming time series. For more information, check out [this notebook]()
+
+## **The Metadata**
+
+A given time series is assigned a unique metadata label configuration which includes user-specified and default labels. We used some basic visualization techniques for metadata analysis including graphing and dimensionality reduction.
+
+One of the default labels that occurs in every metric and metadata packet is the instance. The instance is an ip which corresponds to the scraped target. However, these ip addresses are refreshed over time. We can see this as new instances pop up every once in a while.
+![](../../jupyter-notebooks/data-science-on-prom-metrics/imgs/kubelet_docker_instance_label.png)
+
+![](../../jupyter-notebooks/data-science-on-prom-metrics/imgs/kubelet_docker_op_type_label.png)
+![](../../jupyter-notebooks/data-science-on-prom-metrics/imgs/t-sne_embedding.png)
+## **Forecasting**
+#### Exponential Smoothing
+kubelet_docker_operation_latency_microseconds_quantile
+![](../../jupyter-notebooks/data-science-on-prom-metrics/imgs/exp_smoothing.png)
+#### ARIMA Modelling
+![](../../jupyter-notebooks/data-science-on-prom-metrics/imgs/arima.png)
+#### Prophet Modelling
+#### Fourier Extrapolation
+#### Further Research
+RNNs (LSTMs)
+
+## **Anomaly Detection Decision Rules**
+## **Results**
+Comparing Prophet and Fourier
+## **Conclusion**
---
+++ exp_smoothing_train.py
@@ -0,0 +1,106 @@
+import pickle
+import numpy as np
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+import collections
+import argparse
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+
+
+class Exp_Smoothing:
+        def __init__(self, train, test):
+            self.train = np.array(train["values"])
+            self.ds_train = np.array(train["timestamps"])
+            self.test = np.array(test["values"])
+            self.ds_test = np.array(test["timestamps"])
+
+        def fit_model(self, n_predict):
+
+                fit = SimpleExpSmoothing(self.train).fit()
+                forecast = fit.forecast(n_predict)
+
+                ds = self.ds_test
+                
+                self.forecast = pd.DataFrame({"ds": ds, "yhat": forecast})
+
+                return self.forecast
+
+        def graph(self, metric_name, key):
+                plt.figure(figsize=(40,10))
+                
+                plt.plot(np.array(self.forecast["ds"]), np.array(self.forecast["yhat"]), 'y', label = 'yhat')
+                plt.plot(self.ds_train, self.train, '*b', label = 'train', linewidth = 3)
+                plt.plot(self.ds_test, self.test, '*g', label = 'test', linewidth = 3)
+                # pl.plot(np.array(self.forecast["ds"]), np.array(self.forecast["yhat_upper"]), 'y', label = 'yhat_upper')
+                # pl.plot(np.array(self.forecast["ds"]), np.array(self.forecast["yhat_lower"]), 'y', label = 'yhat_lower')
+                
+                plt.legend()
+                plt.savefig("../testing/exp_smoothing_graphs/graph_" + metric_name + "_" + str(key) + ".png")
+                plt.show()
+
+
+
+def calc_delta(vals):
+        diff = vals - np.roll(vals, 1)
+        diff[0] = 0
+        return diff
+
+def monotonically_inc(vals):
+        # check corner case
+        if len(vals) == 1:
+                return True
+        diff = calc_delta(vals)
+        diff[np.where(vals == 0)] = 0
+
+        if ((diff < 0).sum() == 0):
+                return True
+        else:
+                return False
+
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="frun Prophet training on time series")
+
+        parser.add_argument("--metric", type=str, help='metric name', required=True)
+
+        parser.add_argument("--key", type=int, help='key number')
+        
+        args = parser.parse_args()
+
+        metric_name = args.metric
+
+        pkl_file = open("../pkl_data/" + metric_name + "_dataframes.pkl", "rb")
+        dfs = pickle.load(pkl_file)
+        pkl_file.close()
+        key_vals = list(dfs.keys())
+
+        selected = [args.key]
+        for ind in selected:
+                key = key_vals[ind]
+                df = dfs[key]
+                df = df.sort_values(by=['timestamps'])
+
+                print(key)
+                df["values"] = df["values"].apply(pd.to_numeric)
+                vals = np.array(df["values"].tolist())
+
+                # check if metric is a counter, if so, run AD on difference
+                if monotonically_inc(vals):
+                        print("monotonically_inc")
+                        vals = calc_delta(vals)
+                        df["values"] = vals
+                
+                train = df[0:int(0.7*len(vals))]
+                test = df[int(0.7*len(vals)):]
+
+                es = Exp_Smoothing(train, test)
+                forecast = es.fit_model(test.shape[0])
+                
+                f = open("../testing/exp_smoothing_forecasts/forecast_" + metric_name + "_" + str(args.key) + ".pkl", "wb")
+                pickle.dump(forecast, f)
+                pickle.dump(train, f)
+                pickle.dump(test,f)
+                f.close()
+
+                es.graph(metric_name, args.key)
